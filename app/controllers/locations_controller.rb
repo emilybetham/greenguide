639dc4b58e4c6dfe6e7cb975c9ff02c4ee778a9d
Dania Aflak <dania.aflak@skema.edu>
2019-02-25 14:39:52 +0100

    pundi sur index et home

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,11 +1,14 @@
 class LocationsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
+    @locations = policy_scope(Location)
   end
 
   def new
   end
 
   def create
+    authorize @location
   end
 end
