4e46da9b02be918aaa31942386e9fc36e65b0e8c
Emily Betham <emily.betham@bath.edu>
2019-03-01 15:06:44 +0100

    working on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,165 +28,172 @@ puts 'destroyed all'
 
 locations_attributes = [
   {
-    name:         'Julia\'s skate',
+    name:         'Point recyclage Paris',
     category:     'recyclage',
     address:      '22 rue de Sèvres, 75006 Paris',
-    description:  'Lovely skate',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Esapce recylclage pour toutes sortes de déchets',
+    photo:        'https://www.ecosia.org/images?q=point+recyclage+paris#id=50AC305164C6BCE04553BC5090A73BD000A5ED00'
   },
   {
-    name:         'Julia\'s scooter',
+    name:         'Point recyclage Paris',
     category:     'recyclage',
     address:      '19 rue de Turenne, 75004 Paris',
-    description:  'Lovely trotinette',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Esapce recylclage pour toutes sortes de déchets',
+    photo:        'https://www.ecosia.org/images?q=point+recyclage+paris#id=50AC305164C6BCE04553BC5090A73BD000A5ED00'
   },
   {
-    name:         'Julia\'s palm',
+    name:         'Point recyclage Paris',
+    category:     'recyclage',
+    address:      '32 rue Leon Frot, 75011 Paris',
+    description:  'Esapce recylclage pour toutes sortes de déchets',
+    photo:        'https://www.ecosia.org/images?q=point+recyclage+paris#id=50AC305164C6BCE04553BC5090A73BD000A5ED00'
+  },
+  {
+    name:         'Point recyclage Paris',
     category:     'recyclage',
     address:      '9 rue d\'Orsel, 75018 Paris',
-    description:  'Nice palme in Piccadilly',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Esapce recylclage pour toutes sortes de déchets',
+    photo:        'https://www.ecosia.org/images?q=point+recyclage+paris#id=50AC305164C6BCE04553BC5090A73BD000A5ED00'
   },
   {
-    name:         'Celia\'s roller',
+    name:         'Point recyclage Paris',
     category:     'recyclage',
     address:      '14 Rue Crespin du Gast, 75011 Paris',
-    description:  'Beautiful roller skate',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Esapce recylclage pour toutes sortes de déchets',
+    photo:        'https://www.ecosia.org/images?q=point+recyclage+paris#id=50AC305164C6BCE04553BC5090A73BD000A5ED00'
   },
   {
-    name:         'Dania\'s skate',
+    name:         'Point recyclage Paris',
     category:     'recyclage',
     address:      '209 rue Saint Maur, 75010 Paris',
-    description:  'A beautiful skate',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Esapce recylclage pour toutes sortes de déchets',
+    photo:        'https://www.ecosia.org/images?q=point+recyclage+paris#id=50AC305164C6BCE04553BC5090A73BD000A5ED00'
   },
   {
-    name:         'Emily\'s bike',
+    name:         'Point composte Paris',
     category:     'composte',
     address:      '9 rue d\'orsel',
-    description:  'Try my bike!',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Venez composter avec vos voisins, tous les jours de la semaine.',
+    photo:        'https://www.ecosia.org/images?q=espace+composte+a+paris#id=3AFF585DDB46CCA0864136F55459DF9E563CE12C'
   },
   {
-    name:         'Guillaume\'s skateboard',
+    name:         'Point composte Paris',
     category:     'composte',
     address:      '53 rue sauffroy, 75017 Paris',
-    description:  'A beautiful skate',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Venez composter avec vos voisins, tous les jours de la semaine.',
+    photo:        'https://www.ecosia.org/images?q=espace+composte+a+paris#id=3AFF585DDB46CCA0864136F55459DF9E563CE12C'
   },
   {
-    name:         'Jule\'s bike',
+    name:         'Point composte Paris',
     category:     'composte',
     address:      '4, Rue de la Fontaine Bouillante, 02810, Saint-Gengoulph, Aisne, Hauts-de-France',
-    description:  'Try my bike!',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Venez composter avec vos voisins, tous les jours de la semaine.',
+    photo:        'https://www.ecosia.org/images?q=espace+composte+a+paris#id=3AFF585DDB46CCA0864136F55459DF9E563CE12C'
   },
   {
-    name:         'Marine\'s bike',
+    name:         'Point composte Paris',
     category:     'composte',
     address:      '108, Le Vernay, 71250, Chérizet, Saône-et-Loire, Bourgogne-Franche-Comté',
-    description:  'Try my bike!',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Venez composter avec vos voisins, tous les jours de la semaine.',
+    photo:        'https://www.ecosia.org/images?q=espace+composte+a+paris#id=3AFF585DDB46CCA0864136F55459DF9E563CE12C'
   },
   {
-    name:        'Elie\'s skate',
+    name:        'Friperie de Paris',
     category:     'vêtement',
     address:      '101, La Bâche, Mavilly-Mandelot, Côte-d\'Or, Bourgogne-Franche-Comté',
-    description:  'A beautiful skate',
-    photo:        'https://source.unsplash.com/random'
+    description:  'La meilleur fiperie de Paris! Venez decouvrir votre côté groovy!',
+    photo:        'https://www.ecosia.org/images?q=vintage+clothing+store#id=E272E4C5A2386C514DF0EE10931B22F39DA606F0'
   },
   {
-    name:         'Coralie\'s scooter',
+    name:         'Friperie de Paris',
     category:     'vêtement',
     address:      '196, La Reparerie, 36340, Mouhers, Indre, Centre-Val de Loire',
-    description:  'Lovely trotinette',
-    photo:        'https://source.unsplash.com/random'
+    description:  'La meilleur fiperie de Paris! Venez decouvrir votre côté groovy!',
+    photo:        'https://www.ecosia.org/images?q=vintage+clothing+store#id=E272E4C5A2386C514DF0EE10931B22F39DA606F0'
   },
   {
-    name:         'Hamza\'s palm',
+    name:         'Friperie de Paris',
     category:     'vêtement',
     address:      '4, Rue de la Poste, 37340, Ambillou, Indre-et-Loire, Centre-Val de Loire',
-    description:  'Nice palm in Piccadilly',
-    photo:        'https://source.unsplash.com/random'
+    description:  'La meilleur fiperie de Paris! Venez decouvrir votre côté groovy!',
+    photo:        'https://www.ecosia.org/images?q=vintage+clothing+store#id=E272E4C5A2386C514DF0EE10931B22F39DA606F0'
   },
   {
-    name:         'Germain\'s skateboard',
+    name:         'Magasin Bio',
     category:     'alimentation',
     address:      '5, Route de la Gorgere, 87800, Saint-Hilaire-les-Places, Haute-Vienne, Nouvelle-Aquitaine',
-    description:  'Beautiful skateboard!',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Magasin bio avec plein de choix et possibilité d\'acheter en vrac!',
+    photo:        'https://www.ecosia.org/images?q=magasin+bio#id=E5D5DA1A9DA26DD3A0C3FEFC6F68294B08BBD147'
   },
   {
-    name:         'Louise\'s surf',
+    name:         'Magasin Bio',
     category:     'alimentation',
     address:      '65, 73130, Saint-Colomban-des-Villards, Auvergne-Rhône-Alpes',
-    description:  'My beautiful surf',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Le meilleur magasin bio, sans plastique!',
+    photo:        'https://www.ecosia.org/images?q=magasin+bio#id=E5D5DA1A9DA26DD3A0C3FEFC6F68294B08BBD147'
   },
   {
-    name:         'Claire\'s bike',
+    name:         'Magasin Bio',
     category:     'alimentation',
     address:      '10, Rue de la Cavée, 80160, Fransures, Somme, Hauts-de-France',
-    description:  'Try my bike!',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Le meilleur magasin bio, sans plastique!',
+    photo:        'https://www.ecosia.org/images?q=magasin+bio#id=E5D5DA1A9DA26DD3A0C3FEFC6F68294B08BBD147'
   },
   {
-    name:         'Pierre\'s skateboard',
+    name:         'Marche',
     category:     'marché',
     address:      '8, Rue de la Hutte Baret, 88220, Raon-aux-Bois, Vosges, Grand Est',
-    description:  'A beautiful skate',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Les meilleurs fruits et legumes, tous les matins!',
+    photo:        "https://www.ecosia.org/images?q=march\%C3\%A9+paris#id=C86E64DAB4BA6A61905CE8B7B7507B92984A60F"
   },
   {
-    name:         'Valentine\'s bike',
+    name:         'Marche',
     category:     'marché',
     address:      '16 villa Gaudelet, 75011 Paris',
-    description:  'Try my bike!',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Les meilleurs fruits et legumes, tous les matins!',
+    photo:        "https://www.ecosia.org/images?q=march\%C3\%A9+paris#id=C86E64DAB4BA6A61905CE8B7B7507B92984A60F"
   },
   {
-    name:         'Theo\'s surf',
+    name:         'Marche',
     category:     'marché',
     address:      'Domaine de Pixérécourt, 54220 Malzéville',
-    description:  'My beautiful surf',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Les meilleurs fruits et legumes, tous les matins!',
+    photo:        "https://www.ecosia.org/images?q=march\%C3\%A9+paris#id=C86E64DAB4BA6A61905CE8B7B7507B92984A60F"
   },
   {
-    name:         'Alexandre\'s bike',
+    name:         'Marché',
     category:     'marché',
     address:      'Langhärdle 4, 77784 Oberharmersbach, Allemagne',
-    description:  'Try my bike!',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Marché du dimanche à ne pas rater!',
+    photo:        "https://www.ecosia.org/images?q=march\%C3\%A9+paris#id=C86E64DAB4BA6A61905CE8B7B7507B92984A60F"
   },
   {
-    name:         'Dylan\'s skate',
+    name:         'Festival ecolo',
     category:     'événement',
     address:      '56 rue Sedaine, 75011 Paris',
-    description:  'Lovely skate',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Festival de musique et d\'art, tous les fonds seront donné aux associations ecologiques.',
+    photo:        'https://www.ecosia.org/images?q=we+love+green#id=D3B7F02E95C9934E6209E0967BEA7D66CEAADA6C'
   },
   {
-    name:         'Claire\'s skate',
+    name:         'Soirée ecolo',
     category:     'événement',
     address:      '27 rue Leon Frot, 75011 Paris',
-    description:  'Lovely skate',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Super soirée pour discuter/echanger sur l\'ecologie! Venez nombreux!',
+    photo:        'https://www.ecosia.org/images?q=we+love+green#id=D3B7F02E95C9934E6209E0967BEA7D66CEAADA6C'
   },
   {
-    name:         'Leonie\'s skate',
+    name:         'Talk ecolo',
     category:     'événement',
     address:      '19 rue du Temple, 75004 Paris',
-    description:  'Lovely skate',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Venez ecouter les professionels en ecologie de Paris!',
+    photo:        'https://www.ecosia.org/images?q=we+love+green#id=D3B7F02E95C9934E6209E0967BEA7D66CEAADA6C'
   },
   {
-    name:         'Jad\'s skate',
+    name:         'Marche pour l\'environnement,
     category:     'événement',
     address:      '9 rue jean louis renardon, 69650 Saint Germain au mont d\'or',
-    description:  'Lovely skate',
-    photo:        'https://source.unsplash.com/random'
+    description:  'Marchons ensemble pour proteger notre environnement!',
+    photo:        'https://www.ecosia.org/images?q=we+love+green#id=D3B7F02E95C9934E6209E0967BEA7D66CEAADA6C'
   },
 ]
 
