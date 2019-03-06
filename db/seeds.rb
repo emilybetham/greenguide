faf98dd2327ea7b7feb62c29d8888f9b87ee42b1
Dania Aflak <dania.aflak@skema.edu>
2019-03-06 17:05:45 +0100

    suppression de la categorie evenement dans seed et categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -339,41 +339,6 @@ locations_attributes = [
     description:  'Boutique vintage où l’on pèse ses fringues comme les légumes du marché.',
     photo:        'https://images.unsplash.com/photo-1520006403909-838d6b92c22e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
   },
-  {
-    name:         'Festival écolo',
-    category:     'événement',
-    address:      '56 rue Sedaine, 75011 Paris',
-    description:  'Festival de musique et d\'art, tous les fonds seront donné aux associations ecologiques.',
-    photo:        'https://images.unsplash.com/photo-1472653816316-3ad6f10a6592?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80'
-  },
-  {
-    name:         'Soirée écolo',
-    category:     'événement',
-    address:      '11 Rue Crespin du Gast, 75011 Paris',
-    description:  'Super soirée pour discuter/echanger sur l\'ecologie! Venez nombreux!',
-    photo:        'https://images.unsplash.com/photo-1472653816316-3ad6f10a6592?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80'
-  },
-  {
-    name:         'Soirée écolo',
-    category:     'événement',
-    address:      '27 rue Leon Frot, 75011 Paris',
-    description:  'Super soirée pour discuter/echanger sur l\'ecologie! Venez nombreux!',
-    photo:        'https://images.unsplash.com/photo-1472653816316-3ad6f10a6592?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80'
-  },
-  {
-    name:         'Talk écolo',
-    category:     'événement',
-    address:      '19 rue du Temple, 75004 Paris',
-    description:  'Venez ecouter les professionels en ecologie de Paris!',
-    photo:        'https://images.unsplash.com/photo-1472653816316-3ad6f10a6592?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80'
-  },
-  {
-    name:         'Marche pour l\'environnement',
-    category:     'événement',
-    address:      '9 rue jean louis renardon, 69650 Saint Germain au mont d\'or',
-    description:  'Marchons ensemble pour proteger notre environnement!',
-    photo:        'https://images.unsplash.com/photo-1472653816316-3ad6f10a6592?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80'
-  },
 ]
 
 locations_attributes.each do |attribute|
