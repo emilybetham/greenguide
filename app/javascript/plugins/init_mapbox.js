a5e70b7841411c9b86c554594d3eea9232ebd8f0
Emily Betham <emily.betham@bath.edu>
2019-03-07 17:36:25 +0100

    fixed man

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -115,7 +115,7 @@ const bindMarkersToRoute = (map, userCoordinates) => {
 
             const tripInstructions = [];
             console.log(btnItinerary.dataset.logo)
-            alert(instructions.innerHTML = '🌱 Durée du trajet: ' + Math.floor(data.routes[0].legs[0].duration / 60) + ' minutes 🚶‍♀🚶‍♂');
+            alert(instructions.innerHTML = '🌱 Durée du trajet: ' + Math.floor(data.routes[0].legs[0].duration / 60) + ' minutes🚶‍');
           });
         const close = document.querySelector(`${cardModalId} #card-close-modal`).click();
       });
