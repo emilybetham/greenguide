5678d9f980887eeb478704093fff36e78774ee8d
Emily Betham <emily.betham@bath.edu>
2019-05-02 18:28:10 +0200

    working on the js for toutes categories button

--- app/javascript/components/css_toutes_categories.js
+++ app/javascript/components/css_toutes_categories.js
@@ -1,20 +1,36 @@
+// const cssToutesCategories = () => {
+//   const categories = document.querySelector(".category-choices");
+//   if (categories) {
+//     const toutesCategories = categories.children[1];
+//     toutesCategories.addEventListener('click', (event) => {
+//       const categoriesButtons = document.querySelectorAll('.category-choice');
+//       let i;
+//       for (i = 1; i < categoriesButtons.length; i++) {
+//         // console.log(categoriesButtons[i])
+//         categoriesButtons[i].classList.add('new-class-for-js-of-all-categories-autoselect')
+//         // WHERE IS THE DOUBLE CLICK!?!??!
+//       }
+//       // BELOW WORKS BUT DE-CLICKS THE TOUTES CATEGORIES BUTTON
+//       // categoriesButtons.forEach((categorie) => {
+//       //   categorie.click();
+//       // })
+//     })
+//   }
+// }
+
 const cssToutesCategories = () => {
-  const categories = document.querySelector(".category-choices");
-  if (categories) {
-    const toutesCategories = categories.children[1];
-    toutesCategories.addEventListener('click', (event) => {
-      const categoriesButtons = document.querySelectorAll('.category-choice');
-      let i;
-      for (i = 1; i < 6; i++) {
-        console.log(categoriesButtons[i])
-        categoriesButtons[i].click();
-        // WHERE IS THE DOUBLE CLICK!?!??!
-      }
-      // categoriesButtons.forEach((categorie) => {
-      //   categorie.click();
-      // })
-    })
-  }
+  const toutesCategoriesButton = document.getElementById('search_categories_toutes catégories');
+  toutesCategoriesButton.addEventListener('click', (event) => {
+    const categoriesButtons = document.querySelectorAll('.category-choice');
+    categoriesButtons.forEach((categorie) => {
+      // NEED TO ADD SOMETHING WHICH DE-SELECTS IF THERE IS ALREADY A CURRENT FILTER, BCAUSE OTHERWISE IT STAYS EVEN AFTER TOGGLE!
+      categorie.classList.add('new-class-to-unselect-exisitng-selection')
+      categorie.classList.toggle('new-class-for-js-of-all-categories-autoselect');
+
+    });
+  });
 }
 
+
 export { cssToutesCategories };
+
